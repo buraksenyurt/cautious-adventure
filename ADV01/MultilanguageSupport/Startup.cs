810b9305a32d0d7d2528a2ce3518acb8439b54ff
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-09-20 21:52:12 +0300

    Multilang service added.

--- ADV01/MultilanguageSupport/Startup.cs
+++ ADV01/MultilanguageSupport/Startup.cs
@@ -1,11 +1,13 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -23,7 +25,31 @@ namespace MultilanguageSupport
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            // Çoklu dil desteği için middleware tarafına gerekli servis eklenir
+            services.AddLocalization(options =>
+            {
+                options.ResourcesPath = "Resources"; // çoklu dil desteğine ait dosyaların duracağı klasör
+            });
+
+            services.AddControllersWithViews()
+                .AddViewLocalization(); // View tarafı için çoklu dil desteği veren servis eklenir
+
+            // Çoklu dil desteği için gerekli kültür ayarları yapılır.
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                options.DefaultRequestCulture = new RequestCulture("en-US");
+
+                // İngilizce, Türkçe ve İspanyolca dilleri desteklenecek
+                var cultures = new CultureInfo[]
+                {
+                    new CultureInfo("en-US"),
+                    new CultureInfo("tr-TR"),
+                    new CultureInfo("es-ES")
+                };
+
+                options.SupportedCultures = cultures;
+                options.SupportedUICultures = cultures;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +65,7 @@ namespace MultilanguageSupport
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseRequestLocalization(); // Çoklu dil desteği etkinleştirilir
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
