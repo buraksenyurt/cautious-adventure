faf23cb100ca07d84ef98a36fa0f930e46f56075
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-09-20 22:01:55 +0300

    ViewData support added.

--- ADV01/MultilanguageSupport/Controllers/HomeController.cs
+++ ADV01/MultilanguageSupport/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using MultilanguageSupport.Models;
 using System;
@@ -12,14 +13,20 @@ namespace MultilanguageSupport.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        // Metin içeriklerine çoklu dil desteğini IStringLocalizer tipini Controller sınıfına enjekte ederek kullanabiliriz
+        private readonly IStringLocalizer<HomeController> _stringLocalizer;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IStringLocalizer<HomeController> stringLocalizer)
         {
             _logger = logger;
+            _stringLocalizer = stringLocalizer;
         }
 
         public IActionResult Index()
         {
+            // ViewData üstünden resx dosyalarındaki değerleri View tarafına taşıyabiliriz
+            ViewData["PageTitle"] = _stringLocalizer["page.title"].Value;
+            ViewData["PageDescription"] = _stringLocalizer["page.description"].Value;
             return View();
         }
 
