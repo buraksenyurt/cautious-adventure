59315cf5ba0c5a2d9fb95029684831da3e73ad56
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-09-20 22:40:31 +0300

    Runtime lang change support added.

---
+++ ADV01/MultilanguageSupport/Controllers/LanguageController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MultilanguageSupport.Controllers
+{
+    public class LanguageController
+        : Controller
+    {
+        /*
+         Uygulamanın dilini değiştirmek için _Layout sayfasına eklediğimiz action bu metodu HTTP Post ile çağırır.
+         İstemciye dönecek olan Cookie'ye seçilen Culture bilgisi eklenir.
+         */ 
+        [HttpPost]
+        public IActionResult ChangeLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                    new CookieOptions
+                    {
+                        Expires = DateTimeOffset.UtcNow.AddDays(1)
+                    }
+            );
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
