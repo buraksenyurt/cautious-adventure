357b91a102a809996866c0d1e418c9d63f7dd193
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-09-20 22:22:51 +0300

    IViewLocalizer usage added.

--- ADV01/MultilanguageSupport/Controllers/HomeController.cs
+++ ADV01/MultilanguageSupport/Controllers/HomeController.cs
@@ -2,11 +2,7 @@
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using MultilanguageSupport.Models;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MultilanguageSupport.Controllers
 {
