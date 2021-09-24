ab814adf60a4411d51336779141c7dec960f830e
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-09-24 20:51:31 +0300

    FluentValidation packages added.

--- ADV03/SmoothValidation/Startup.cs
+++ ADV03/SmoothValidation/Startup.cs
@@ -1,13 +1,10 @@
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace SmoothValidation
 {
@@ -23,6 +20,10 @@ namespace SmoothValidation
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddFluentValidation(v =>
+            {
+                v.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            });
             services.AddControllersWithViews();
         }
 
