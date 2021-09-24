77974e6d7ac47a26fd9b34e11c1c14e0f661dabf
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-09-24 21:55:32 +0300

    Mapping profile class added.

---
+++ ADV03/EasyMapping/Mappings/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using EasyMapping.Domain;
+using EasyMapping.Models;
+
+namespace EasyMapping.Mappings
+{
+    /*
+     * AutoMapper nesne dönüşümleri esnasında bir yol haritasına ihtiyaç duyar.
+     * Kimi kime dönüştüreceğini bilmek için bu haritayı kullanır.
+     */
+    public class MappingProfile
+        :Profile
+    {
+        // Harita Profile tipinden türeyen bir sınıfın yapıcı metodunda oluşur.
+        public MappingProfile()
+        {
+            // Eşleştirilecek ne kadar nesne varsa burada belirtilir.
+            CreateMap<Player, PlayerModel>();
+        }
+    }
+}
