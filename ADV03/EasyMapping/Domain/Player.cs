01771c7366038a8195e6bcda5ef4b2c365f450f9
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-09-24 21:54:35 +0300

    Model and entity objects added.

---
+++ ADV03/EasyMapping/Domain/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EasyMapping.Domain
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string Nickname { get; set; }
+        public short Level { get; set; }
+        public int? Age { get; set; }
+        public string FirstBonusPack { get; set; }
+        public DateTime? SubscriptionDate { get; set; }
+    }
+}
---
+++ ADV03/EasyMapping/Models/PlayerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EasyMapping.Models
+{
+    public class PlayerModel
+    {
+        public int PlayerId { get; set; }
+        public string Nickname { get; set; }
+        public short Level { get; set; }
+        public int? Age { get; set; }
+        public string FirstBonusPack { get; set; }
+        public DateTime? SubscriptionDate { get; set; }
+    }
+}
