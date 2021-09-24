a6ab116280cf4e52bf097fef97f556202a47b4bc
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-09-24 20:56:03 +0300

    Model and controller added.

---
+++ ADV03/SmoothValidation/Controllers/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmoothValidation.Models;
+
+namespace SmoothValidation.Controllers
+{
+    public class PlayerController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Player player)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(player);
+            }
+            // Gelen Player nesne verisi doğrulama adımlarını başarı ile tamamlamışsa buraya gelinir.
+            // Burada repository'ye kaydetme işlemleri yapılır. Örneğin konusu açısından şimdilik gerekli değil.
+            return Ok();
+        }
+    }
+}
---
+++ ADV03/SmoothValidation/Models/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace SmoothValidation.Models
+{
+    public class Player
+    {
+        public string Nickname { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string MobilePhone { get; set; }
+        public int Level { get; set; }
+    }
+}
